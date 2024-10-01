85b36bea97b3cf43e32047aedbac41ad843eb16d
EwaKulpinska <eva.kulpinska@gmail.com>
2024-10-01 14:49:55 +0200

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="hello">
+		<h1>This is the Home page</h1>
+		<button @click="sendRequest('http://localhost:5002/info')">
+			Check sensor
+		</button>
+	</div>
+</template>
+<script setup>
+async function sendRequest(url) {
+	const response = await fetch(url, {
+		method: "GET",
+		mode: "cors",
+		headers: { "Content-Type": "application/json" },
+	});
+	const jsonResponse = await response.json();
+	console.log(jsonResponse);
+	return jsonResponse;
+}
+</script>
+
+<style>
+.hello {
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
